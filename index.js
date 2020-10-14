01ebb864671f356ad538482e565ce47a850a1ddd
Robert Grimes <rjgrimes@gmail.com>
2020-10-14 14:16:27 -0500

    Adjusted index.js to account for testing before release

--- index.js
+++ index.js
@@ -8,46 +8,46 @@ const license = ["Apache License 2.0", "BSD 3-Clause", "BSD 2-Clause", "GNU Gene
 
 // array of questions for user
 const questions = [{
-        type: "input",
-        message: "What is the name of your project?",
-        name: "projectName"
-    },
-    {
-        type: "input",
-        message: "What description would you like for your project",
-        name: "description"
-    },
-    {
-        type: "input",
-        message: "What, if any, are the installation instructions for this program?",
-        name: "installGuide"
-    },
-    {
-        type: "input",
-        message: "How does the user use this program?",
-        name: "usageGuide"
-    },
-    {
-        type: "input",
-        message: "Who helped collaborate with this project? (Type their GitHub User Name",
-        name: "collaborators"
-    },
-    {
-        type: "choice",
-        message: "Which license would you like to use?",
-        choices: license,
-        name: "selectedLicense"
-    },
-    {
-        type: "input",
-        message: "What is your github name?",
-        name: "creatorGithub"
-    },
-    {
-        type: "input",
-        message: "What is your email address?",
-        name: "creatorEmail"
-    }
+    type: "input",
+    message: "What is the name of your project?",
+    name: "projectName"
+},
+{
+    type: "input",
+    message: "What description would you like for your project",
+    name: "description"
+},
+{
+    type: "input",
+    message: "What, if any, are the installation instructions for this program?",
+    name: "installGuide"
+},
+{
+    type: "input",
+    message: "How does the user use this program?",
+    name: "usageGuide"
+},
+{
+    type: "input",
+    message: "Who helped collaborate with this project? (Type their GitHub User Name",
+    name: "collaborators"
+},
+{
+    type: "choice",
+    message: "Which license would you like to use?",
+    choices: license,
+    name: "selectedLicense"
+},
+{
+    type: "input",
+    message: "What is your github name?",
+    name: "creatorGithub"
+},
+{
+    type: "input",
+    message: "What is your email address?",
+    name: "creatorEmail"
+}
 ];
 
 // function to write README file
@@ -61,10 +61,10 @@ async function init() {
     // put inquirer inside here
     try {
         const data = await
-        inquirer
-            .prompt(questions);
-
-        // writeToFile(/*name*/,data);
+            inquirer
+                .prompt(questions);
+        console.log(data.collaborators);
+        // writeToFile(data);
     } catch (err) {
         confirm.log("Error" + err);
     }
