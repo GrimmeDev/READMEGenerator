73bb6603bb2b02da26538ad8d42779cc229c8847
Robert Grimes <rjgrimes@gmail.com>
2020-10-15 17:01:22 -0500

    Adjusted formating of template literal

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -3,6 +3,7 @@ function generateMarkdown(data) {
         // console.log("In Generate");
         // console.log(data);
         const collabs = data.collaborators.split(/[.,\/ -]/);
+        //#region Psuedo Thinking
         // DESTRUCTURE data(?)
         // May want to write checks to see if collaborators is NOT empty, if empty return a different template?
         // If not empty, split(?) data.collaborators into different segments to link to their github profiles?
@@ -16,28 +17,42 @@ function generateMarkdown(data) {
         Credits
         License
         Badges
-    
         */
+        //#endregion
         return `# ${data.projectName}
 ## Description
 ${data.description}
 ## Table of Contents
+<ol>
+<li>[Description](#Description)</li>
+<li>[Installation](#Installation)</li>
+<li>[Usage](#Usage)</li>
+<li>[License](#License)</li>
+${collabs != "" ?
+                        `<li>[Collaborators](#Collaborators)</li>` : ""}
+<li>[Testing](#Testing)</li>
+<li>[Questions](#Questions)</li>
+${data.specialMsg ?
+                        `<li>[Special](#Special)</li>` : ""}
+</ol>
 
-### Installation
+## Installation
 ${data.installGuide}
-### Usage
+## Usage
 ${data.usageGuide}
-#### License
+### License
 ${data.selectedLicense}
 ${collabs != "" ?
-                        `#### Collaborators \n
+                        `#### Collaborators
 ${collabs.map(collabs => `[${" " + collabs}]("https://github.com/"${collabs})`)}` : ""}
-#### Testing
+### Testing
 ${data.testingGuide}
 #### Questions?
 If you have questions regarding this program:
-Send me a message through GitHub here [![GitHub Link](https://img.shields.io/badge/Github-${data.creatorGithub}-lightgrey.svg)](https://github.com/${data.creatorGithub}) or through email here [email link]
-##### Credits
+Send me a message through GitHub here [![GitHub Link](https://img.shields.io/badge/Github-${data.creatorGithub}-lightgrey.svg)](https://github.com/${data.creatorGithub}) or through email here [![Email Link](https://img.shields.io/badge/EMAIL-ME-lightgrey.svg)](${data.creatorEmail})
+${data.specialMsg ?
+                        `#### Special Message\n
+${data.specialMsg}}` : ""}
 
 `;
 }
